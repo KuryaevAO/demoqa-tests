c2adc3f25708607a445b725f1db7e1bc959ac4fc
KuryaevAO <aleksandr.kuryaev@simbirsoft.com>
2023-05-23 12:38:46 +0300

    added attachments

--- src/test/java/com/kuryaevao/tests/TestBase.java
+++ src/test/java/com/kuryaevao/tests/TestBase.java
@@ -3,8 +3,10 @@ package com.kuryaevao.tests;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import helpers.Attach;
 
 public class TestBase {
 
@@ -25,5 +27,13 @@ public class TestBase {
 
         Configuration.browserCapabilities = capabilities;
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub/";
+    }
+
+    @AfterEach
+    public void tearDown() {
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
     }
 }
